46cfe00dcb0562474e4bee98d006518d16bd84f2
Praveen rai <Siddhartha-90>
2024-01-10 18:07:24 +0530

    first commit

---
+++ Public/script.js
@@ -0,0 +1,31 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var newUserLink = document.getElementById('newUser');
+//     var existingUserLink = document.getElementById('existingUser');
+
+//     newUserLink.addEventListener('click', function () {
+//         // Redirect to the sign-up page for new users
+//         window.location.href = 'signup.html';
+//     });
+
+//     existingUserLink.addEventListener('click', function () {
+//         // Redirect to the login page for existing users
+//         window.location.href = 'login.html';
+//     });
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var newUserLink = document.getElementById('newUserLink');
+//     var existingUserLink = document.getElementById('existingUser');
+
+//     newUserLink.addEventListener('click', function () {
+//         // Redirect to the sign-up page for new users
+//         window.location.href = 'signup.html';
+//     });
+
+//     existingUserLink.addEventListener('click', function () {
+//         // Redirect to the login page for existing users
+//         window.location.href = 'login.html';
+//     });
+// });
+
+
